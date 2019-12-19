6a3040676ebbda9e88faeaa2fd3b3ba29d710d39
William Queiroz <wnqueiroz@outlook.com>
2019-12-19 16:49:18 -0300

    Criando estilo

--- exercicios/src/components/simple.js
+++ exercicios/src/components/simple.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
+import defaultComponents from '../styles/defaultComponents';
 
 export default function Simple({ text }) {
-  return <Text>It's a {text ? text : 'simple'} component!</Text>;
+  return (
+    <Text style={defaultComponents.ex}>
+      It's a {text ? text : 'simple'} component!
+    </Text>
+  );
 }
